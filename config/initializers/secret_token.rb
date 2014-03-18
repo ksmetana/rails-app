8311d1937c7d14c4b3c3ebd191ab3180e32b5d00
EC2 Default User <ec2-user@ip-172-31-46-37.us-west-2.compute.internal>
2014-03-18 02:23:34 +0000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitte::Application.config.secret_key_base = '39d6ecb884e6fddce7da874d52be1a23c540e7556fe20057e3dcdda99813faa336a8a8f7e3a666c9e72b3416126e5895d1df445c2317153838e089ded9241a46'
